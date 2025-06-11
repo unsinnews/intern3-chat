ad881814e3390e23400e40f2a1b9d1f7bdbd1947
Sahaj Jain <sahajjain13@gmail.com>
2025-06-11 14:33:01 +0530

    feat: Add rehype-sanitise

--- components/memoized-markdown.tsx
+++ components/memoized-markdown.tsx
@@ -2,7 +2,52 @@ import { marked } from "marked";
 import { memo, useMemo } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeSanitize from "rehype-sanitize";
+import type { Schema } from "hast-util-sanitize";
 import { Codeblock } from "./codeblock";
+
+// Custom sanitization schema
+const sanitizeSchema: Schema = {
+  tagNames: [
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "p",
+    "blockquote",
+    "ul",
+    "ol",
+    "li",
+    "strong",
+    "em",
+    "del",
+    "code",
+    "pre",
+    "hr",
+    "br",
+    "a",
+    "img",
+    "table",
+    "thead",
+    "tbody",
+    "tr",
+    "th",
+    "td",
+  ],
+  attributes: {
+    "*": ["className", "id", "data-theme"],
+    a: ["href", "title", "target", "rel"],
+    img: ["src", "alt", "title", "width", "height"],
+    card: ["title", "subtext", "largeText", "id", "caption"],
+    financialchart: ["*"],
+  },
+  protocols: {
+    href: ["http", "https", "mailto"],
+    src: ["http", "https"],
+  },
+};
 
 function parseMarkdownIntoBlocks(markdown: string): string[] {
   const tokens = marked.lexer(markdown);
@@ -14,6 +59,7 @@ const MemoizedMarkdownBlock = memo(
     return (
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
+        rehypePlugins={[[rehypeSanitize, sanitizeSchema]]}
         components={{
           code: Codeblock,
         }}
